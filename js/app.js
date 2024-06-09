1b81fd0353176ecbc99a2b373ef02b2a5b9256fe
QodirovaNozima1 <nozimaqodirova07@gmail.com>
2024-06-10 02:01:12 +0500

    math

---
+++ js/app.js
@@ -0,0 +1,104 @@
+//  {
+    //masala 01
+    // function perimetr(a) {
+        // return 4 * a
+    // }
+    // console.log(perimetr(7));
+    // console.log(perimetr(17));
+//  }
+
+{
+    //masala 02
+    // function yuzi(a) {
+        // return s = a * a
+    // }
+    // console.log(yuzi(2));
+}
+{
+    // masala 3
+    // function perimetr() {
+            // function yuza() {   
+                // return a * b
+            // }
+            // console.log( yuza());
+            // return 2 * ( a + b)
+        // }
+        // console.log(perimetr());
+}
+
+{
+    // masala 4
+    // let d = prompt("d diametr")
+    // alert("uzunligi consoleda")
+    // function uzunlik(d){
+        // return d * 3.14
+    // }
+    // console.log(uzunlik(8));
+}
+
+{
+    // masala 5
+    // let a = prompt("a soniini kiritng")
+    // alert("hajmi va yuzasi consoleda")
+    // function hajmi(a){
+    //     function yuzasi(a){
+    //         return 6 * a * a
+    //     }
+    //     console.log(yuzasi(8));
+    //     return a * a * a
+    // }
+    // console.log(hajmi(6));
+}
+
+
+{
+    // masala 6
+    // function hajmi(a,b,c){
+        // function yuzasi(a,b,c){
+            // return 2 * (a * b + b * c + c * a)
+        // }
+        // console.log(yuzasi(5,7,2));
+        // return a * b * c
+    // }
+    // console.log(hajmi(5,7,2));
+}
+
+
+
+{
+    //masala 07
+    // function radius(R) {
+        // let L = 2 * 3.14 * R
+        // let S = 3.14 * R * R
+        // return `uzunligi ${L} , yuzasi ${S}`
+    // }
+    // console.log( radius (4) );
+}
+
+{
+    //masala 08
+    // function arifmetigi( a, b) {
+        // return (a + b)/2
+    // }
+    // console.log(arifmetigi (4, 6) );
+}
+{
+    //masala 09
+    // function ildiz(a , b) {
+        // return Math.sqrt(a * b)
+    // }
+    // console.log( ildiz (-6 ,2) );
+}
+
+{
+    //masala 10
+    // function kvadrati( a, b) {
+        // let yegindi = a + b
+        // let kopaytma = a * b
+        // let kvadrat = a * a 
+        // let kvadrat1 = b * b
+        // return `yegindi ${yegindi}, kopaytma ${kopaytma}, a kvadrati ${kvadrat}, b kvadrati ${kvadrat1} `
+    // }
+    // console.log(kvadrati ( 3, 8) );
+
+}
